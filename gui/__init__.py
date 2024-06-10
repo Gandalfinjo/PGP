932049eb76fd555ce5684a7bb0d9b9a250769868
Dragan <draganceperic@gmail.com>
2024-06-10 22:36:51 +0200

    Created Main Window GUI

---
+++ gui/__init__.py
@@ -0,0 +1,14 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMainWindow
+
+from gui.main_window import Ui_MainWindow
+
+
+class PGPApp(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+        self.connect()
+
+    def connect(self):
+        pass
--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello")
+from sys import argv, exit
+from PyQt5.QtWidgets import QApplication
+from gui import PGPApp
+
+
+if __name__ == "__main__":
+    app = QApplication(argv)
+    win = PGPApp()
+    win.show()
+    exit(app.exec_())
