ce529c875326d78dc5bbf15092bbafc6cb082ea7
Dragan <draganceperic@gmail.com>
2024-06-11 10:29:29 +0200

    Changed the Main Window GUI

--- gui/__init__.py
+++ gui/__init__.py
@@ -8,7 +8,7 @@ class PGPApp(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
-        self.connect()
+        self.setup_connections()
 
-    def connect(self):
+    def setup_connections(self):
         pass
